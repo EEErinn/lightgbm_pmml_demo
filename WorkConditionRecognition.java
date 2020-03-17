96241fd78a41fa855e3227e13b925452adcb01b4
EEErinn <50447810+EEErinn@users.noreply.github.com>
2020-03-17 09:31:17 +0800

    Java's lightgbm predictive interface
    Use Java to create lightgbm's predictive interface

---
+++ WorkConditionRecognition.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.dmg.pmml.FieldName;
+import org.dmg.pmml.PMML;
+import org.jpmml.evaluator.Evaluator;
+import org.jpmml.evaluator.FieldValue;
+import org.jpmml.evaluator.InputField;
+import org.jpmml.evaluator.ModelEvaluator;
+import org.jpmml.evaluator.ModelEvaluatorFactory;
+import org.jpmml.evaluator.TargetField;
+
+public class WorkConditionRecognition {
+}
